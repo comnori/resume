69d5e5a9fbad9706bf194d9f6b89b497b268c626
Yongsik Yun <comnori@gmail.com>
2023-12-18 01:29:10 +0900

    Fixing add yarn global path
    ## Added
    
    add editor default font to d2coding ligature
    add terminal default font to d2coding ligature Nerd Font Mono
    add font.ligature to true
    
    ## Changed
    
    postCreateCommands merge to init.sh

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -1,8 +1,22 @@
 #/bin/sh
 sudo chsh -s /bin/zsh $(whoami)
+
 sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" \
   -t jonathan \
   -p git \
   -p ssh-agent \
   -p https://github.com/zsh-users/zsh-autosuggestions \
   -p https://github.com/zsh-users/zsh-completions
+
+corepack enable
+
+yarn global add gatsby-cli
+
+cat << EOF >> ~/.profile
+# set PATH so it includes user's private bin if it exists
+if [ -d "\$HOME/.yarn/bin" ] ; then
+  PATH="\$(yarn global bin):\$PATH"
+fi
+EOF
+
+source ~/.profile
