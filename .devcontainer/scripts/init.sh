8eac1f29c49b84c92e5b9e1225a3e4bc09539ba6
윤용식 <comnori@gmail.com>
2023-12-18 12:02:59 +0900

    Fix incorrect zsh-in-docker installation script. yarn global path set.

--- .devcontainer/scripts/init.sh
+++ .devcontainer/scripts/init.sh
@@ -1,10 +1,10 @@
 #/bin/sh
-sudo chsh -s /bin/zsh $(whoami)
-
-sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" \
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.5/zsh-in-docker.sh)" -- \
   -t jonathan \
   -p git \
   -p ssh-agent \
+  -p yarn \
+  -p gatsby \
   -p https://github.com/zsh-users/zsh-autosuggestions \
   -p https://github.com/zsh-users/zsh-completions
 
@@ -12,11 +12,11 @@ corepack enable
 
 yarn global add gatsby-cli
 
-cat << EOF >> ~/.profile
-# set PATH so it includes user's private bin if it exists
+cat << EOF >> ~/.zshrc
+
+# set PATH so it includes user's yarn global bin if it exists
 if [ -d "\$HOME/.yarn/bin" ] ; then
   PATH="\$(yarn global bin):\$PATH"
 fi
 EOF
 
-source ~/.profile
