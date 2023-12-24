d70618d67be6702c8f2d2c660e29eb874a337025
윤용식 <comnori@gmail.com>
2023-12-24 20:30:12 +0900

    Update gatsby-config.ts, package.json, and yarn.lock

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -11,7 +11,6 @@ module.exports = {
     siteUrl: `https://comnori.github.com/resume`,
   },
   plugins: [
-    "gatsby-plugin-theme-ui",
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
