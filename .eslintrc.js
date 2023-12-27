5ea59792e7df5c97509b3ecc0529cfb46d40bab7
윤용식 <comnori@gmail.com>
2023-12-28 02:59:38 +0900

    Gatsby 의 경우 eslintrc.js 를 통해 설정할 경우 Browser console 혹은 gatsby develop 시의 eslint 설정이 무시됨.
    vscode 에서 eslint를 확장해서 사용할 경우에는 gatsby 의 설정을 무시하거나 gatsby의 설정을 대부분 포팅해와야 함.
    
    eslint를 중복으로 돌리는 것 보다는 gatsby rule을 먼저 따르고 추가적인 사항이 필요할 경우에 다시 고려하는 것이 좋을 것으로 판단됨.

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
-  extends: `react-app`,
-  settings: {
-    linkComponents: ["Hyperlink", { name: "Link", linkAttribute: "href" }],
-  },
-}
